49d900ff1b77114145217e258455121e598f510b
Aimeos <aimeos@aimeos.org>
2015-01-11 14:32:00 +0100

    Minor improvements

--- Entity/User.php
+++ Entity/User.php
@@ -40,7 +40,7 @@ class User implements UserInterface, \Serializable
     /**
      * @ORM\Column(name="status", type="integer")
      */
-    private $isActive = true;
+    private $isActive;
 
 
     /**
@@ -118,6 +118,6 @@ class User implements UserInterface, \Serializable
             $this->id,
             $this->username,
             $this->password,
-        ) = unserialize($serialized);
+        ) = unserialize( $serialized );
     }
 }
