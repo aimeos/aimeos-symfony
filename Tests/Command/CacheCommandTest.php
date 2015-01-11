890ff7a4dd506d9de6dcb70b99e0d01c00835671
Aimeos <aimeos@aimeos.org>
2015-01-11 18:52:17 +0100

    Implement cache command test

---
+++ Tests/Command/CacheCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Aimeos\ShopBundle\Command;
+
+
+class CacheCommandTest extends WebTestCase
+{
+    public function testCacheCommand()
+    {
+        $kernel = $this->createKernel();
+        $kernel->boot();
+
+        $application = new Application( $kernel );
+        $application->add( new Command\CacheCommand() );
+
+        $command = $application->find( 'aimeos:cache' );
+        $commandTester = new CommandTester( $command );
+        $commandTester->execute( array( 'command' => $command->getName(), 'site' => 'unittest' ) );
+
+        $this->assertEquals( 0, $commandTester->getStatusCode() );
+    }
+}
