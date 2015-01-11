77657570006c9b45a49dcf02d57be9d1b7ce8641
Aimeos <aimeos@aimeos.org>
2015-01-11 15:38:57 +0100

    Implemented command for clearing the content cache

---
+++ Command/CacheCommand.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * @license MIT, http://opensource.org/licenses/MIT
+ * @copyright Aimeos (aimeos.org), 2014
+ */
+
+
+namespace Aimeos\ShopBundle\Command;
+
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+/**
+ * Clears the content cache
+ */
+class CacheCommand extends Command
+{
+	/**
+	 * Configures the command name and description.
+	 */
+	protected function configure()
+	{
+		$this->setName( 'aimeos:cache' );
+		$this->setDescription( 'Clears the content cache' );
+		$this->addArgument( 'site', InputArgument::OPTIONAL, 'Site codes to clear the cache like "default unittest" (none for all)' );
+		$this->addOption( 'extdir', null, InputOption::VALUE_OPTIONAL, 'Directory containing additional Aimeos extensions' );
+		$this->addOption( 'config', null, InputOption::VALUE_OPTIONAL, 'Directory containing additional configuration' );
+	}
+
+
+	/**
+	 * Executes the job controllers.
+	 *
+	 * @param InputInterface $input Input object
+	 * @param OutputInterface $output Output object
+	 */
+	protected function execute( InputInterface $input, OutputInterface $output )
+	{
+		$extDir = $input->getOption( 'extdir' );
+		$arcavias = new \Arcavias( ( $extDir ? (array) $extDir : array() ) );
+
+		$adapter = $this->getContainer()->getParameter( 'database_driver' );
+		$adapter = str_replace( 'pdo_', '', $adapter );
+
+		$i18nPaths = $arcavias->getI18nPaths();
+		$configPaths = $arcavias->getConfigPaths( $adapter );
+
+		if( ( $confPath = $input->getOption( 'config' ) ) !== null ) {
+			$configPaths[] = $confPath;
+		}
+
+		$context = $this->getContext( $configPaths, $i18nPaths );
+		$localeManager = \MShop_Factory::createManager( $context, 'locale' );
+
+		foreach( $this->getSiteItems( $context, $input ) as $siteItem )
+		{
+			$localeItem = $localeManager->bootstrap( $siteItem->getCode(), '', '', false );
+
+			$lcontext = clone $context;
+			$lcontext->setLocale( $localeItem );
+
+			\MAdmin_Cache_Manager_Factory::createManager( $lcontext )->getCache()->flush();
+		}
+	}
+
+
+	/**
+	 * Returns the enabled site items which may be limited by the input arguments.
+	 *
+	 * @param \MShop_Context_Item_Interface $context Context item object
+	 * @param InputInterface $input Input object
+	 * @return \MShop_Locale_Item_Site_Interface[] List of site items
+	 */
+	protected function getSiteItems( \MShop_Context_Item_Interface $context, InputInterface $input )
+	{
+		$manager = \MShop_Factory::createManager( $context, 'locale/site' );
+		$search = $manager->createSearch();
+		$expr = array();
+
+		if( ( $codes = $input->getArgument( 'site' ) ) != null ) {
+			$search->setConditions( $search->compare( '==', 'locale.site.code', explode( ' ', $codes ) ) );
+		}
+
+		return $manager->searchItems( $search );
+	}
+}
