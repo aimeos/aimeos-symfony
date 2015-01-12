5ae55efaf0a0590e05416d2deb969721910a597b
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-01-12 11:44:40 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- Command/CacheCommand.php
+++ Command/CacheCommand.php
@@ -8,7 +8,6 @@
 
 namespace Aimeos\ShopBundle\Command;
 
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
