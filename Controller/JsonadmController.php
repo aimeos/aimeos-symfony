1996652680e2df25e7a76f092ac46ce893eb20ca
Aimeos <aimeos@aimeos.org>
2015-11-16 19:23:34 +0100

    Implemented JSON API controller and tests

---
+++ Controller/JsonadmController.php
@@ -0,0 +1,224 @@
+<?php
+
+/**
+ * @license MIT, http://opensource.org/licenses/MIT
+ * @copyright Aimeos (aimeos.org), 2015
+ * @package symfony2-bundle
+ * @subpackage Controller
+ */
+
+
+namespace Aimeos\ShopBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+/**
+ * Aimeos controller for the JSON REST API
+ *
+ * @package symfony2-bundle
+ * @subpackage Controller
+ */
+class JsonadmController extends Controller
+{
+	/**
+	 * Deletes the resource object or a list of resource objects
+	 *
+	 * @param \Symfony\Component\HttpFoundation\Request $request Request object
+	 * @param string $sitecode Unique site code
+	 * @param string Resource location, e.g. "product/stock/wareshouse"
+	 * @param integer|null $id Unique resource ID
+	 * @return \Symfony\Component\HttpFoundation\Response Response object containing the generated output
+	 */
+	public function deleteAction( Request $request, $site, $resource, $id = null )
+	{
+		$status = 500;
+		$header = $request->headers->all();
+
+		$cntl = $this->createController( $site, $resource, $request->get( 'lang', 'en' ) );
+		$result = $cntl->delete( $request->getContent(), $header, $status );
+
+		return $this->createResponse( $result, $status, $header );
+	}
+
+
+	/**
+	 * Returns the requested resource object or list of resource objects
+	 *
+	 * @param \Symfony\Component\HttpFoundation\Request $request Request object
+	 * @param string $sitecode Unique site code
+	 * @param string Resource location, e.g. "product/stock/wareshouse"
+	 * @param integer|null $id Unique resource ID
+	 * @return \Symfony\Component\HttpFoundation\Response Response object containing the generated output
+	 */
+	public function getAction( Request $request, $site, $resource, $id = null )
+	{
+		$status = 500;
+		$header = $request->headers->all();
+
+		$cntl = $this->createController( $site, $resource, $request->get( 'lang', 'en' ) );
+		$result = $cntl->get( $request->getContent(), $header, $status );
+
+		return $this->createResponse( $result, $status, $header );
+	}
+
+
+	/**
+	 * Updates a resource object or a list of resource objects
+	 *
+	 * @param \Symfony\Component\HttpFoundation\Request $request Request object
+	 * @param string $sitecode Unique site code
+	 * @param string Resource location, e.g. "product/stock/wareshouse"
+	 * @param integer|null $id Unique resource ID
+	 * @return \Symfony\Component\HttpFoundation\Response Response object containing the generated output
+	 */
+	public function patchAction( Request $request, $site, $resource, $id = null )
+	{
+		$status = 500;
+		$header = $request->headers->all();
+
+		$cntl = $this->createController( $site, $resource, $request->get( 'lang', 'en' ) );
+		$result = $cntl->patch( $request->getContent(), $header, $status );
+
+		return $this->createResponse( $result, $status, $header );
+	}
+
+
+	/**
+	 * Creates a new resource object or a list of resource objects
+	 *
+	 * @param \Symfony\Component\HttpFoundation\Request $request Request object
+	 * @param string $sitecode Unique site code
+	 * @param string Resource location, e.g. "product/stock/wareshouse"
+	 * @param integer $id Unique ID of the resource
+	 * @return \Symfony\Component\HttpFoundation\Response Response object containing the generated output
+	 */
+	public function postAction( Request $request, $site, $resource, $id = null )
+	{
+		$status = 500;
+		$header = $request->headers->all();
+
+		$cntl = $this->createController( $site, $resource, $request->get( 'lang', 'en' ) );
+		$result = $cntl->post( $request->getContent(), $header, $status );
+
+		return $this->createResponse( $result, $status, $header );
+	}
+
+
+	/**
+	 * Creates or updates a single resource object
+	 *
+	 * @param \Symfony\Component\HttpFoundation\Request $request Request object
+	 * @param string $sitecode Unique site code
+	 * @param string Resource location, e.g. "product/stock/wareshouse"
+	 * @param integer|null $id Unique resource ID
+	 * @return \Symfony\Component\HttpFoundation\Response Response object containing the generated output
+	 */
+	public function putAction( Request $request, $site, $resource, $id = null )
+	{
+		$status = 500;
+		$header = $request->headers->all();
+
+		$cntl = $this->createController( $site, $resource, $request->get( 'lang', 'en' ) );
+		$result = $cntl->put( $request->getContent(), $header, $status );
+
+		return $this->createResponse( $result, $status, $header );
+	}
+
+
+	/**
+	 * Returns the available HTTP verbs and the resource URLs
+	 *
+	 * @param \Symfony\Component\HttpFoundation\Request $request Request object
+	 * @param string $sitecode Unique site code
+	 * @param string Resource location, e.g. "product/stock/wareshouse"
+	 * @return \Symfony\Component\HttpFoundation\Response Response object containing the generated output
+	 */
+	public function optionsAction( Request $request, $site = 'default', $resource = '' )
+	{
+		$status = 500;
+		$header = $request->headers->all();
+
+		$cntl = $this->createController( $site, $resource, $request->get( 'lang', 'en' ) );
+		$result = $cntl->options( $request->getContent(), $header, $status );
+
+		return $this->createResponse( $result, $status, $header );
+	}
+
+
+	/**
+	 * Returns the resource controller
+	 *
+	 * @param string $sitecode Unique site code
+	 * @param string Resource location, e.g. "product/stock/wareshouse"
+	 * @param string $lang Language code
+	 * @return \Aimeos\MShop\Context\Item\Iface Context item
+	 */
+	protected function createController( $sitecode, $resource, $lang )
+	{
+		$aimeos = $this->get( 'aimeos' )->get();
+		$templatePaths = $aimeos->getCustomPaths( 'controller/jsonadm/templates' );
+
+		$context = $this->get( 'aimeos_context' )->get( false );
+		$context = $this->setLocale( $context, $sitecode, $lang );
+
+		$view = $this->get('aimeos_view')->create( $context->getConfig(), $templatePaths, $lang );
+		$context->setView( $view );
+
+		return \Aimeos\Controller\JsonAdm\Factory::createController( $context, $templatePaths, $resource );
+	}
+
+
+	/**
+	 * Creates a new response object
+	 *
+	 * @param string $content Body of the HTTP response
+	 * @param integer $status HTTP status
+	 * @param array $header List of HTTP headers
+	 * @return \Illuminate\Http\Response HTTP response object
+	 */
+	protected function createResponse( $content, $status, array $header )
+	{
+		$response = new Response();
+		$response->setContent( $content );
+		$response->setStatusCode( $status );
+
+		foreach( $header as $key => $value ) {
+			$response->headers->set( $key, $value );
+		}
+
+		return $response;
+	}
+
+
+	/**
+	 * Sets the locale item in the given context
+	 *
+	 * @param \Aimeos\MShop\Context\Item\Iface $context Context object
+	 * @param string $sitecode Unique site code
+	 * @param string $lang ISO language code, e.g. "en" or "en_GB"
+	 * @return \Aimeos\MShop\Context\Item\Iface Modified context object
+	 */
+	protected function setLocale( \Aimeos\MShop\Context\Item\Iface $context, $sitecode, $lang )
+	{
+		$localeManager = \Aimeos\MShop\Factory::createManager( $context, 'locale' );
+
+		try
+		{
+			$localeItem = $localeManager->bootstrap( $sitecode, '', '', false );
+			$localeItem->setLanguageId( null );
+			$localeItem->setCurrencyId( null );
+		}
+		catch( \Aimeos\MShop\Locale\Exception $e )
+		{
+			$localeItem = $localeManager->createItem();
+		}
+
+		$context->setLocale( $localeItem );
+		$context->setI18n( $this->get('aimeos_i18n')->get( array( $lang ) ) );
+
+		return $context;
+	}
+}
---
+++ Tests/Controller/JsonadmControllerTest.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Aimeos\ShopBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class JsonadmControllerTest extends WebTestCase
+{
+	public function testOptionsAction()
+	{
+		$client = static::createClient();
+
+
+		$client->request( 'OPTIONS', '/unittest/jsonadm/product' );
+		$response = $client->getResponse();
+
+		$json = json_decode( $response->getContent(), true );
+
+		$this->assertNotNull( $json );
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertArrayHasKey( 'resources', $json['meta'] );
+		$this->assertGreaterThan( 1, count( $json['meta']['resources'] ) );
+
+
+		$client->request( 'OPTIONS', '/unittest/jsonadm' );
+		$response = $client->getResponse();
+
+		$json = json_decode( $response->getContent(), true );
+
+		$this->assertNotNull( $json );
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertArrayHasKey( 'resources', $json['meta'] );
+		$this->assertGreaterThan( 1, count( $json['meta']['resources'] ) );
+	}
+
+
+	public function testActionsSingle()
+	{
+		$client = static::createClient();
+
+
+		$content = '{"data":{"type":"product/stock/warehouse","attributes":{"product.stock.warehouse.code":"symfony","product.stock.warehouse.label":"symfony"}}}';
+		$client->request( 'POST', '/unittest/jsonadm/product/stock/warehouse', array(), array(), array(), $content );
+		$response = $client->getResponse();
+
+		$json = json_decode( $response->getContent(), true );
+
+		$this->assertNotNull( $json );
+		$this->assertEquals( 201, $response->getStatusCode() );
+		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data']['attributes'] );
+		$this->assertEquals( 'symfony', $json['data']['attributes']['product.stock.warehouse.code'] );
+		$this->assertEquals( 'symfony', $json['data']['attributes']['product.stock.warehouse.label'] );
+		$this->assertEquals( 1, $json['meta']['total'] );
+
+		$id = $json['data']['attributes']['product.stock.warehouse.id'];
+
+
+		$content = '{"data":{"type":"product/stock/warehouse","attributes":{"product.stock.warehouse.code":"symfony2","product.stock.warehouse.label":"symfony2"}}}';
+		$client->request( 'PATCH', '/unittest/jsonadm/product/stock/warehouse/' . $id, array(), array(), array(), $content );
+		$response = $client->getResponse();
+
+		$json = json_decode( $response->getContent(), true );
+
+		$this->assertNotNull( $json );
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data']['attributes'] );
+		$this->assertEquals( 'symfony2', $json['data']['attributes']['product.stock.warehouse.code'] );
+		$this->assertEquals( 'symfony2', $json['data']['attributes']['product.stock.warehouse.label'] );
+		$this->assertEquals( $id, $json['data']['attributes']['product.stock.warehouse.id'] );
+		$this->assertEquals( 1, $json['meta']['total'] );
+
+
+		$client->request( 'GET', '/unittest/jsonadm/product/stock/warehouse/' . $id );
+		$response = $client->getResponse();
+
+		$json = json_decode( $response->getContent(), true );
+
+		$this->assertNotNull( $json );
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data']['attributes'] );
+		$this->assertEquals( 'symfony2', $json['data']['attributes']['product.stock.warehouse.code'] );
+		$this->assertEquals( 'symfony2', $json['data']['attributes']['product.stock.warehouse.label'] );
+		$this->assertEquals( $id, $json['data']['attributes']['product.stock.warehouse.id'] );
+		$this->assertEquals( 1, $json['meta']['total'] );
+
+
+		$client->request( 'DELETE', '/unittest/jsonadm/product/stock/warehouse/' . $id );
+		$response = $client->getResponse();
+
+		$json = json_decode( $response->getContent(), true );
+
+		$this->assertNotNull( $json );
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertEquals( 1, $json['meta']['total'] );
+	}
+
+
+	public function testActionsBulk()
+	{
+		$client = static::createClient();
+
+
+		$content = '{"data":[
+			{"type":"product/stock/warehouse","attributes":{"product.stock.warehouse.code":"symfony","product.stock.warehouse.label":"symfony"}},
+			{"type":"product/stock/warehouse","attributes":{"product.stock.warehouse.code":"symfony2","product.stock.warehouse.label":"symfony"}}
+		]}';
+		$client->request( 'POST', '/unittest/jsonadm/product/stock/warehouse', array(), array(), array(), $content );
+		$response = $client->getResponse();
+
+		$json = json_decode( $response->getContent(), true );
+
+		$this->assertNotNull( $json );
+		$this->assertEquals( 201, $response->getStatusCode() );
+		$this->assertEquals( 2, count( $json['data'] ) );
+		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data'][0]['attributes'] );
+		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data'][1]['attributes'] );
+		$this->assertEquals( 'symfony', $json['data'][0]['attributes']['product.stock.warehouse.label'] );
+		$this->assertEquals( 'symfony', $json['data'][1]['attributes']['product.stock.warehouse.label'] );
+		$this->assertEquals( 2, $json['meta']['total'] );
+
+		$ids = array( $json['data'][0]['attributes']['product.stock.warehouse.id'], $json['data'][1]['attributes']['product.stock.warehouse.id'] );
+
+
+		$content = '{"data":[
+			{"type":"product/stock/warehouse","id":' . $ids[0] . ',"attributes":{"product.stock.warehouse.label":"symfony2"}},
+			{"type":"product/stock/warehouse","id":' . $ids[1] . ',"attributes":{"product.stock.warehouse.label":"symfony2"}}
+		]}';
+		$client->request( 'PATCH', '/unittest/jsonadm/product/stock/warehouse', array(), array(), array(), $content );
+		$response = $client->getResponse();
+
+		$json = json_decode( $response->getContent(), true );
+
+		$this->assertNotNull( $json );
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertEquals( 2, count( $json['data'] ) );
+		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data'][0]['attributes'] );
+		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data'][1]['attributes'] );
+		$this->assertEquals( 'symfony2', $json['data'][0]['attributes']['product.stock.warehouse.label'] );
+		$this->assertEquals( 'symfony2', $json['data'][1]['attributes']['product.stock.warehouse.label'] );
+		$this->assertTrue( in_array( $json['data'][0]['attributes']['product.stock.warehouse.id'], $ids ) );
+		$this->assertTrue( in_array( $json['data'][1]['attributes']['product.stock.warehouse.id'], $ids ) );
+		$this->assertEquals( 2, $json['meta']['total'] );
+
+
+		$getParams = ['filter' => ['&&' => [
+			['=~' => ['product.stock.warehouse.code' => 'symfony']],
+			['==' => ['product.stock.warehouse.label' => 'symfony2']]
+			]],
+			'sort' => 'product.stock.warehouse.code', 'page' => ['offset' => 0, 'limit' => 3]
+		];
+		$client->request( 'GET', '/unittest/jsonadm/product/stock/warehouse', $getParams );
+		$response = $client->getResponse();
+
+		$json = json_decode( $response->getContent(), true );
+
+		$this->assertNotNull( $json );
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertEquals( 2, count( $json['data'] ) );
+		$this->assertEquals( 'symfony', $json['data'][0]['attributes']['product.stock.warehouse.code'] );
+		$this->assertEquals( 'symfony2', $json['data'][1]['attributes']['product.stock.warehouse.code'] );
+		$this->assertEquals( 'symfony2', $json['data'][0]['attributes']['product.stock.warehouse.label'] );
+		$this->assertEquals( 'symfony2', $json['data'][1]['attributes']['product.stock.warehouse.label'] );
+		$this->assertTrue( in_array( $json['data'][0]['attributes']['product.stock.warehouse.id'], $ids ) );
+		$this->assertTrue( in_array( $json['data'][1]['attributes']['product.stock.warehouse.id'], $ids ) );
+		$this->assertEquals( 2, $json['meta']['total'] );
+
+
+		$content = '{"data":[
+			{"type":"product/stock/warehouse","id":' . $ids[0] . '},
+			{"type":"product/stock/warehouse","id":' . $ids[1] . '}
+		]}';
+		$client->request( 'DELETE', '/unittest/jsonadm/product/stock/warehouse', array(), array(), array(), $content );
+		$response = $client->getResponse();
+
+		$json = json_decode( $response->getContent(), true );
+
+		$this->assertNotNull( $json );
+		$this->assertEquals( 200, $response->getStatusCode() );
+		$this->assertEquals( 2, $json['meta']['total'] );
+	}
+}
