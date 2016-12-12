bd47bb1329c8947bda7d232dd102b47763255def
Aimeos <aimeos@aimeos.org>
2016-12-12 11:37:13 +0100

    Adapted tests to stock being in own domain now

--- Tests/Controller/JsonadmControllerTest.php
+++ Tests/Controller/JsonadmControllerTest.php
@@ -46,52 +46,52 @@ class JsonadmControllerTest extends WebTestCase
 		) );
 
 
-		$content = '{"data":{"type":"product/stock/warehouse","attributes":{"product.stock.warehouse.code":"symfony","product.stock.warehouse.label":"symfony"}}}';
-		$client->request( 'POST', '/unittest/jsonadm/product/stock/warehouse', array(), array(), array(), $content );
+		$content = '{"data":{"type":"stock/type","attributes":{"stock.type.code":"symfony","stock.type.label":"symfony"}}}';
+		$client->request( 'POST', '/unittest/jsonadm/stock/type', array(), array(), array(), $content );
 		$response = $client->getResponse();
 
 		$json = json_decode( $response->getContent(), true );
 
 		$this->assertNotNull( $json );
 		$this->assertEquals( 201, $response->getStatusCode() );
-		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data']['attributes'] );
-		$this->assertEquals( 'symfony', $json['data']['attributes']['product.stock.warehouse.code'] );
-		$this->assertEquals( 'symfony', $json['data']['attributes']['product.stock.warehouse.label'] );
+		$this->assertArrayHasKey( 'stock.type.id', $json['data']['attributes'] );
+		$this->assertEquals( 'symfony', $json['data']['attributes']['stock.type.code'] );
+		$this->assertEquals( 'symfony', $json['data']['attributes']['stock.type.label'] );
 		$this->assertEquals( 1, $json['meta']['total'] );
 
-		$id = $json['data']['attributes']['product.stock.warehouse.id'];
+		$id = $json['data']['attributes']['stock.type.id'];
 
 
-		$content = '{"data":{"type":"product/stock/warehouse","attributes":{"product.stock.warehouse.code":"symfony2","product.stock.warehouse.label":"symfony2"}}}';
-		$client->request( 'PATCH', '/unittest/jsonadm/product/stock/warehouse/' . $id, array(), array(), array(), $content );
+		$content = '{"data":{"type":"stock/type","attributes":{"stock.type.code":"symfony2","stock.type.label":"symfony2"}}}';
+		$client->request( 'PATCH', '/unittest/jsonadm/stock/type/' . $id, array(), array(), array(), $content );
 		$response = $client->getResponse();
 
 		$json = json_decode( $response->getContent(), true );
 
 		$this->assertNotNull( $json );
 		$this->assertEquals( 200, $response->getStatusCode() );
-		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data']['attributes'] );
-		$this->assertEquals( 'symfony2', $json['data']['attributes']['product.stock.warehouse.code'] );
-		$this->assertEquals( 'symfony2', $json['data']['attributes']['product.stock.warehouse.label'] );
-		$this->assertEquals( $id, $json['data']['attributes']['product.stock.warehouse.id'] );
+		$this->assertArrayHasKey( 'stock.type.id', $json['data']['attributes'] );
+		$this->assertEquals( 'symfony2', $json['data']['attributes']['stock.type.code'] );
+		$this->assertEquals( 'symfony2', $json['data']['attributes']['stock.type.label'] );
+		$this->assertEquals( $id, $json['data']['attributes']['stock.type.id'] );
 		$this->assertEquals( 1, $json['meta']['total'] );
 
 
-		$client->request( 'GET', '/unittest/jsonadm/product/stock/warehouse/' . $id );
+		$client->request( 'GET', '/unittest/jsonadm/stock/type/' . $id );
 		$response = $client->getResponse();
 
 		$json = json_decode( $response->getContent(), true );
 
 		$this->assertNotNull( $json );
 		$this->assertEquals( 200, $response->getStatusCode() );
-		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data']['attributes'] );
-		$this->assertEquals( 'symfony2', $json['data']['attributes']['product.stock.warehouse.code'] );
-		$this->assertEquals( 'symfony2', $json['data']['attributes']['product.stock.warehouse.label'] );
-		$this->assertEquals( $id, $json['data']['attributes']['product.stock.warehouse.id'] );
+		$this->assertArrayHasKey( 'stock.type.id', $json['data']['attributes'] );
+		$this->assertEquals( 'symfony2', $json['data']['attributes']['stock.type.code'] );
+		$this->assertEquals( 'symfony2', $json['data']['attributes']['stock.type.label'] );
+		$this->assertEquals( $id, $json['data']['attributes']['stock.type.id'] );
 		$this->assertEquals( 1, $json['meta']['total'] );
 
 
-		$client->request( 'DELETE', '/unittest/jsonadm/product/stock/warehouse/' . $id );
+		$client->request( 'DELETE', '/unittest/jsonadm/stock/type/' . $id );
 		$response = $client->getResponse();
 
 		$json = json_decode( $response->getContent(), true );
@@ -111,10 +111,10 @@ class JsonadmControllerTest extends WebTestCase
 
 
 		$content = '{"data":[
-			{"type":"product/stock/warehouse","attributes":{"product.stock.warehouse.code":"symfony","product.stock.warehouse.label":"symfony"}},
-			{"type":"product/stock/warehouse","attributes":{"product.stock.warehouse.code":"symfony2","product.stock.warehouse.label":"symfony"}}
+			{"type":"stock/type","attributes":{"stock.type.code":"symfony","stock.type.label":"symfony"}},
+			{"type":"stock/type","attributes":{"stock.type.code":"symfony2","stock.type.label":"symfony"}}
 		]}';
-		$client->request( 'POST', '/unittest/jsonadm/product/stock/warehouse', array(), array(), array(), $content );
+		$client->request( 'POST', '/unittest/jsonadm/stock/type', array(), array(), array(), $content );
 		$response = $client->getResponse();
 
 		$json = json_decode( $response->getContent(), true );
@@ -122,20 +122,20 @@ class JsonadmControllerTest extends WebTestCase
 		$this->assertNotNull( $json );
 		$this->assertEquals( 201, $response->getStatusCode() );
 		$this->assertEquals( 2, count( $json['data'] ) );
-		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data'][0]['attributes'] );
-		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data'][1]['attributes'] );
-		$this->assertEquals( 'symfony', $json['data'][0]['attributes']['product.stock.warehouse.label'] );
-		$this->assertEquals( 'symfony', $json['data'][1]['attributes']['product.stock.warehouse.label'] );
+		$this->assertArrayHasKey( 'stock.type.id', $json['data'][0]['attributes'] );
+		$this->assertArrayHasKey( 'stock.type.id', $json['data'][1]['attributes'] );
+		$this->assertEquals( 'symfony', $json['data'][0]['attributes']['stock.type.label'] );
+		$this->assertEquals( 'symfony', $json['data'][1]['attributes']['stock.type.label'] );
 		$this->assertEquals( 2, $json['meta']['total'] );
 
-		$ids = array( $json['data'][0]['attributes']['product.stock.warehouse.id'], $json['data'][1]['attributes']['product.stock.warehouse.id'] );
+		$ids = array( $json['data'][0]['attributes']['stock.type.id'], $json['data'][1]['attributes']['stock.type.id'] );
 
 
 		$content = '{"data":[
-			{"type":"product/stock/warehouse","id":' . $ids[0] . ',"attributes":{"product.stock.warehouse.label":"symfony2"}},
-			{"type":"product/stock/warehouse","id":' . $ids[1] . ',"attributes":{"product.stock.warehouse.label":"symfony2"}}
+			{"type":"stock/type","id":' . $ids[0] . ',"attributes":{"stock.type.label":"symfony2"}},
+			{"type":"stock/type","id":' . $ids[1] . ',"attributes":{"stock.type.label":"symfony2"}}
 		]}';
-		$client->request( 'PATCH', '/unittest/jsonadm/product/stock/warehouse', array(), array(), array(), $content );
+		$client->request( 'PATCH', '/unittest/jsonadm/stock/type', array(), array(), array(), $content );
 		$response = $client->getResponse();
 
 		$json = json_decode( $response->getContent(), true );
@@ -143,22 +143,22 @@ class JsonadmControllerTest extends WebTestCase
 		$this->assertNotNull( $json );
 		$this->assertEquals( 200, $response->getStatusCode() );
 		$this->assertEquals( 2, count( $json['data'] ) );
-		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data'][0]['attributes'] );
-		$this->assertArrayHasKey( 'product.stock.warehouse.id', $json['data'][1]['attributes'] );
-		$this->assertEquals( 'symfony2', $json['data'][0]['attributes']['product.stock.warehouse.label'] );
-		$this->assertEquals( 'symfony2', $json['data'][1]['attributes']['product.stock.warehouse.label'] );
-		$this->assertTrue( in_array( $json['data'][0]['attributes']['product.stock.warehouse.id'], $ids ) );
-		$this->assertTrue( in_array( $json['data'][1]['attributes']['product.stock.warehouse.id'], $ids ) );
+		$this->assertArrayHasKey( 'stock.type.id', $json['data'][0]['attributes'] );
+		$this->assertArrayHasKey( 'stock.type.id', $json['data'][1]['attributes'] );
+		$this->assertEquals( 'symfony2', $json['data'][0]['attributes']['stock.type.label'] );
+		$this->assertEquals( 'symfony2', $json['data'][1]['attributes']['stock.type.label'] );
+		$this->assertTrue( in_array( $json['data'][0]['attributes']['stock.type.id'], $ids ) );
+		$this->assertTrue( in_array( $json['data'][1]['attributes']['stock.type.id'], $ids ) );
 		$this->assertEquals( 2, $json['meta']['total'] );
 
 
 		$getParams = array( 'filter' => array( '&&' => array(
-			array( '=~' => array( 'product.stock.warehouse.code' => 'symfony' ) ),
-			array( '==' => array( 'product.stock.warehouse.label' => 'symfony2' ) )
+			array( '=~' => array( 'stock.type.code' => 'symfony' ) ),
+			array( '==' => array( 'stock.type.label' => 'symfony2' ) )
 			) ),
-			'sort' => 'product.stock.warehouse.code', 'page' => array( 'offset' => 0, 'limit' => 3 )
+			'sort' => 'stock.type.code', 'page' => array( 'offset' => 0, 'limit' => 3 )
 		);
-		$client->request( 'GET', '/unittest/jsonadm/product/stock/warehouse', $getParams );
+		$client->request( 'GET', '/unittest/jsonadm/stock/type', $getParams );
 		$response = $client->getResponse();
 
 		$json = json_decode( $response->getContent(), true );
@@ -166,20 +166,20 @@ class JsonadmControllerTest extends WebTestCase
 		$this->assertNotNull( $json );
 		$this->assertEquals( 200, $response->getStatusCode() );
 		$this->assertEquals( 2, count( $json['data'] ) );
-		$this->assertEquals( 'symfony', $json['data'][0]['attributes']['product.stock.warehouse.code'] );
-		$this->assertEquals( 'symfony2', $json['data'][1]['attributes']['product.stock.warehouse.code'] );
-		$this->assertEquals( 'symfony2', $json['data'][0]['attributes']['product.stock.warehouse.label'] );
-		$this->assertEquals( 'symfony2', $json['data'][1]['attributes']['product.stock.warehouse.label'] );
-		$this->assertTrue( in_array( $json['data'][0]['attributes']['product.stock.warehouse.id'], $ids ) );
-		$this->assertTrue( in_array( $json['data'][1]['attributes']['product.stock.warehouse.id'], $ids ) );
+		$this->assertEquals( 'symfony', $json['data'][0]['attributes']['stock.type.code'] );
+		$this->assertEquals( 'symfony2', $json['data'][1]['attributes']['stock.type.code'] );
+		$this->assertEquals( 'symfony2', $json['data'][0]['attributes']['stock.type.label'] );
+		$this->assertEquals( 'symfony2', $json['data'][1]['attributes']['stock.type.label'] );
+		$this->assertTrue( in_array( $json['data'][0]['attributes']['stock.type.id'], $ids ) );
+		$this->assertTrue( in_array( $json['data'][1]['attributes']['stock.type.id'], $ids ) );
 		$this->assertEquals( 2, $json['meta']['total'] );
 
 
 		$content = '{"data":[
-			{"type":"product/stock/warehouse","id":' . $ids[0] . '},
-			{"type":"product/stock/warehouse","id":' . $ids[1] . '}
+			{"type":"stock/type","id":' . $ids[0] . '},
+			{"type":"stock/type","id":' . $ids[1] . '}
 		]}';
-		$client->request( 'DELETE', '/unittest/jsonadm/product/stock/warehouse', array(), array(), array(), $content );
+		$client->request( 'DELETE', '/unittest/jsonadm/stock/type', array(), array(), array(), $content );
 		$response = $client->getResponse();
 
 		$json = json_decode( $response->getContent(), true );
