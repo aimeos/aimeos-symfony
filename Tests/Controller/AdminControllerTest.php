344151d8835e907572520b6c8145ad5d83e71e1a
Aimeos <aimeos@aimeos.org>
2017-07-09 13:35:20 +0200

    Removed test because basic auth won't work any more

--- Tests/Controller/AdminControllerTest.php
+++ Tests/Controller/AdminControllerTest.php
@@ -17,19 +17,4 @@ class AdminControllerTest extends WebTestCase
 		$this->assertEquals( 200, $response->getStatusCode() );
 		$this->assertContains( '<form class="login"', $response->getContent() );
 	}
-
-
-	public function testIndexPass()
-	{
-		$client = static::createClient(array(), array(
-			'PHP_AUTH_USER' => 'admin',
-			'PHP_AUTH_PW'   => 'adminpass',
-		) );
-
-		$client->request( 'GET', '/admin' );
-		$response = $client->getResponse();
-
-		$this->assertEquals( 302, $response->getStatusCode() );
-		$this->assertContains( '/default/jqadm/search/dashboard?lang=en', $response->getContent() );
-	}
 }
