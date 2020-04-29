9b4ea51ded1c705eb7fa349a5e84326a44864338
Aimeos <aimeos@aimeos.org>
2020-04-29 12:52:01 +0200

    Don't restart jobs on failure

--- Command/JobsCommand.php
+++ Command/JobsCommand.php
@@ -69,7 +69,7 @@ class JobsCommand extends Command
 					\Aimeos\Controller\Jobs::create( $context, $aimeos, $jobname )->run();
 				};
 
-				$process->start( $fcn, [$context, $aimeos, $jobname], true );
+				$process->start( $fcn, [$context, $aimeos, $jobname], false );
 			}
 		}
 
