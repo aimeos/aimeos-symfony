17c1afffd4a90bf2294222615ee69ae772947ee7
Aimeos <aimeos@aimeos.org>
2023-07-12 11:46:52 +0200

    Removed component actions

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -52,62 +52,6 @@ class AccountController extends Controller
 	public function downloadAction() : Response
 	{
 		$response = $this->container->get( 'shop' )->get( 'account/download' )->getView()->response();
-		return Response::create( (string) $response->getBody(), $response->getStatusCode(), $response->getHeaders() );
-	}
-
-
-	/**
-	 * Returns the output of the account favorite component
-	 *
-	 * @return Response Response object containing the generated output
-	 */
-	public function favoriteComponentAction() : Response
-	{
-		$client = $this->container->get( 'shop' )->get( 'account/favorite' );
-		$this->container->get( 'twig' )->addGlobal( 'aiheader', (string) $client->getHeader() );
-
-		return new Response( (string) $client->getBody() );
-	}
-
-
-	/**
-	 * Returns the output of the account history component
-	 *
-	 * @return Response Response object containing the generated output
-	 */
-	public function historyComponentAction() : Response
-	{
-		$client = $this->container->get( 'shop' )->get( 'account/history' );
-		$this->container->get( 'twig' )->addGlobal( 'aiheader', (string) $client->getHeader() );
-
-		return new Response( (string) $client->getBody() );
-	}
-
-
-	/**
-	 * Returns the output of the account profile component
-	 *
-	 * @return Response Response object containing the generated output
-	 */
-	public function profileComponentAction() : Response
-	{
-		$client = $this->container->get( 'shop' )->get( 'account/profile' );
-		$this->container->get( 'twig' )->addGlobal( 'aiheader', (string) $client->getHeader() );
-
-		return new Response( (string) $client->getBody() );
-	}
-
-
-	/**
-	 * Returns the output of the account watch component
-	 *
-	 * @return Response Response object containing the generated output
-	 */
-	public function watchComponentAction() : Response
-	{
-		$client = $this->container->get( 'shop' )->get( 'account/watch' );
-		$this->container->get( 'twig' )->addGlobal( 'aiheader', (string) $client->getHeader() );
-
-		return new Response( (string) $client->getBody() );
+		return new Response( (string) $response->getBody(), $response->getStatusCode(), $response->getHeaders() );
 	}
 }
