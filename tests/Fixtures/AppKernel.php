4b696e2d228f60aeac2e86e8fd1fb2e8fef08310
Aimeos <aimeos@aimeos.org>
2023-07-15 19:06:49 +0200

    Use config.yaml for tests (renamed from config.yml)

--- tests/Fixtures/AppKernel.php
+++ tests/Fixtures/AppKernel.php
@@ -29,7 +29,7 @@ class AppKernel extends Kernel
 	 */
 	public function registerContainerConfiguration( LoaderInterface $loader )
 	{
-		$loader->load( __DIR__ . '/config/config.yml' );
+		$loader->load( __DIR__ . '/config/config.yaml' );
 	}
 
 
