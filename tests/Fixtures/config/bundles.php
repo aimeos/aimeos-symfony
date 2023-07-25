b6bfd49d19e661d2fa76fcd066c97b5f7ec32e5e
Aimeos <aimeos@aimeos.org>
2023-07-25 08:19:57 +0200

    Added Twig bundle for tests

--- tests/Fixtures/config/bundles.php
+++ tests/Fixtures/config/bundles.php
@@ -3,5 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Aimeos\ShopBundle\AimeosShopBundle::class => ['all' => true],
 ];
