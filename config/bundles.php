b9e0d86b3b9e6c7c00c0c0eb85e7a6cc4ee479e1
Aimeos <aimeos@aimeos.org>
2023-07-25 08:08:18 +0200

    Removed unnecessary bundle configuration

--- config/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Aimeos\ShopBundle\AimeosShopBundle::class => ['all' => true],
-];
