a1f9a58db29bea15b0b7d11f6fcc3f89bf5a7c5d
Aimeos <aimeos@aimeos.org>
2023-07-27 09:47:12 +0200

    Explicitly use new bundle structure

--- src/AimeosShopBundle.php
+++ src/AimeosShopBundle.php
@@ -13,4 +13,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AimeosShopBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return dirname( __DIR__ );
+    }
 }
